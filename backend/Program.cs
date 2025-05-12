0075f278a00c6ac17c3690ad92ffcb0916fc5784
Clara Dujardin <clara.dujardin@supinfo.com>
2025-05-12 15:33:07 +0200

    base complète projet(backend, frontend, bd lié ensemble par Docker)

--- backend/Program.cs
+++ backend/Program.cs
@@ -7,18 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//      app.MapOpenApi();
+// }
 
 app.UseHttpsRedirection();
 
